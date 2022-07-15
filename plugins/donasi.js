31d0597166318f36172fc140b88a8aea131e2c63
FZfortirex <99706613+FZfortirex@users.noreply.github.com>
2022-07-15 19:26:20 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,12 +2,10 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), `
 *Allo mypren bantu donasi yuk supaya bot aktif selalu👋*
 ╔═══════════════════
-║ _*DONASI UNTUK*_  𝙆𝙧𝙞𝙯𝙮𝙣 ×͜×
+║ _*DONASI UNTUK*_  Zain
 ╠═══════════════════
 ║╭──❉ 〔 *𝗗𝗢𝗡𝗔𝗦𝗜* 〕 ❉──────
-║│➸ *DANA* : 081360482998
-║│➸ *PULSA*: 081360482998
-║│➸ *GOPAY*: Lagi Gak pakai
+║│➸ *PULSA*: 085728581059
 ║╰──────────────────
 ╰═══════════════════
 
